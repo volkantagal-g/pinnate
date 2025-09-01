c734d337dea773d1a0513110a393a37b4151f076
Selen Şar <selen.sar@getir.com>
2025-09-01 16:33:14 +0300

    export generate functions

--- src/tokens/elevation/index.ts
+++ src/tokens/elevation/index.ts
@@ -5,3 +5,14 @@ export const elevation = defaultTokens.elevation;
 export const { sm } = elevation;
 
 export type { ElevationTokens } from '../types';
+
+// generate all elevation values as css variables and export them
+export const generateAllElevationCSSVariables = () => {
+  const cssVars: Record<string, string> = {};
+
+  Object.entries(defaultTokens.elevation).forEach(([elevationName, value]) => {
+    cssVars[`--pinnate-elevation-${elevationName}`] = value;
+  });
+
+  return cssVars;
+};
